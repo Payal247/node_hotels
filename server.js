917b700e7c7bc8c3247dd8824135c5c8e833f92f
Payal Yadav <payalyadav241002@gmail.com>
2024-10-06 19:21:51 +0530

    shift mongodb to online server

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 const db = require('./db');
+require('dotenv').config();
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
-
+const PORT = process.env.PORT || 3000;
 
 
 app.get('/', (req, res) => {
@@ -15,8 +16,6 @@ app.get('/', (req, res) => {
 const personRoutes = require('./routes/personRoutes');
 app.use('/person', personRoutes);
 
-
-
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log('listening on port 3000');
 })
